c0b1144acf208ae560675a5d962352fa3f6beaf0
Francis Tyers <spectre@ivixor.net>
2025-08-15 04:29:15 -0600

    add readme

---
+++ cv-corpus-23.0-2025-09-17/update-readme.py
@@ -0,0 +1,31 @@
+import sys, glob
+
+draft_files = glob.glob('draft/en/*.md')
+final_files = glob.glob('final/en/*.md')
+draft_codes = [f.split('/')[-1].split('.')[0] for f in draft_files]
+final_codes = [f.split('/')[-1].split('.')[0] for f in final_files]
+
+draft_codes.sort()
+final_codes.sort()
+
+print('# Datasheets')
+
+print('## Status')
+
+total_count = 0
+final_count = 0
+status = []
+
+status.append('| Draft | Final |')
+status.append('|-------|-------|')
+for code in draft_codes:
+	res = '-'
+	if code in final_codes:
+		res = '✔'
+		final_count += 1
+	status.append('| `%s` | %s |' % (code, res))
+	total_count += 1
+
+print(final_count, '/', total_count)
+print()
+print('\n'.join(status))
